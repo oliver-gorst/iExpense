41a88da44c8c3e551f4421c5595cec1626ec2d0f
Oliver Gorst <oliver.gorst@outlook.com>
2022-01-21 13:47:01 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Oliver Gorst on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
